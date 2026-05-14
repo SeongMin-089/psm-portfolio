92223dc174d37cbb3298680f7cd3dd8fa9f944f1
unknown <a99701841@gmail.com>
2026-05-14 19:21:32 +0900

    project 작업 완

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,37 +1,78 @@
-import React from 'react'
-import projects from '../../utils/project'
-import './styles/Project.scss'
+import React from "react"
+import projects from "../../utils/project"
+import "./styles/Project.scss"
 
 const Project = () => {
   return (
-    <div className="inner work-inner">
-      <h3 className="sub-tit-2">projects.</h3>
+    <section id="Project">
+      <div className="inner project-inner">
+        <div className="section-badge">
+          <span className="section-badge-dot"></span>
+          <span className="section-badge-text">Project</span>
+        </div>
 
-      <ul className="work-lst">
-        {projects.map((project) => (
-          <li key={project.id}>
-            <a href={project.links.notion} className="lst-t-wrap">
-              <h4 className="lst-tit">{project.title}</h4>
+        <div className="project-title-box">
+          <h2>프로젝트 모음</h2>
+          <p>구현 결과를 정리했습니다.</p>
+        </div>
 
-              <p className="lst-dsc">{project.description}</p>
+        <div className="project-list">
+          {projects.map((project, index) => (
+            <article
+              className={`project-card ${index % 2 === 1 ? "reverse" : ""}`}
+              key={project.id}
+            >
+              <a
+                href={project.links.notion}
+                className="project-info"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="project-top">
+                  <span className="project-category">{project.category}</span>
 
-              <div className="tags">
-                {project.tags.map((tag, i) => (
-                  <span key={i}>{tag}</span>
-                ))}
-              </div>
-            </a>
+                  <span className="project-icon">
+                    <img src={project.icon} alt="" />
+                  </span>
+                </div>
 
-            <a href={project.links.demo} className="lst-img-wrap">
-              <div
-                className="bg"
-                style={{ backgroundImage: `url(${project.thumbnail})` }}
-              ></div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+                <div className="project-text">
+                  <h3>{project.title}</h3>
+                  <p>{project.description}</p>
+                </div>
+
+                <div className="tags">
+                  {project.tags.map((tag, i) => (
+                    <span key={i}>
+                      <img src={tag.icon} alt="" />
+                      {tag.name}
+                    </span>
+                  ))}
+                </div>
+
+                <div className="project-bottom">
+                  <span className="project-summary">
+                    <img src={project.summaryIcon} alt="" />
+                    {project.summary}
+                  </span>
+
+                  <strong>PROJECT {String(index + 1).padStart(2, "0")}</strong>
+                </div>
+              </a>
+
+              <a
+                href={project.links.demo}
+                className="project-visual"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={project.thumbnail} alt={`${project.title} preview`} />
+              </a>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
